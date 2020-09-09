6cc13cb4f3163ea7f450f3655b83f5969bb776c2
Danny Oh <dannyoh0316@gmail.com>
2020-09-09 13:16:14 -0500

    Add invalid move blocker in Player.py

--- Player/Player.py
+++ Player/Player.py
@@ -4,30 +4,40 @@ class Player():
 
 
 class HumanPlayer(Player):
+    """
+    Human player. Next move is the move inputted.
+    """
     def __init__(self, board):
         super().__init__(board)
 
     def next_move(self):
         print("Please enter your move:", end = ' ')
         move = int(input())
-        self.board.board_data[move - 1] = self.board.h_letter
+        while self.board.board_data[move - 1] != '-':
+            print("Space already taken. Enter your move:", end = ' ')
+            move = int(input())
+        self.board.board_data[move - 1] = self.board.h_letter            
 
 
 class MinimaxComputerPlayer(Player):
+    """
+    Computer player that chooses next move based on Minimax algorithm.
+    """
     def __init__(self, board):
         super().__init__(board)
 
     def next_move(self):
         bestScore = -99999
         bestMove = 0
+        # Run Minimax on each available move, updating bestMove after each available move tested.
         for move in self.board.available_moves():
-            self.board.board_data[move] = self.board.c_letter
-            score = self.minimax(self.board, False)
-            self.board.board_data[move] = '-'
-            if score > bestScore:
+            self.board.board_data[move] = self.board.c_letter # Make hypothetical move.
+            score = self.minimax(self.board, False) # Get Minimax score for the move.
+            self.board.board_data[move] = '-' # Undo hypothetical move after getting Minimax score.
+            if score > bestScore: # Update bestMove if Minimax score for this move is the highest yet.
                 bestScore = score
                 bestMove = move
-        self.board.board_data[bestMove] = self.board.c_letter
+        self.board.board_data[bestMove] = self.board.c_letter # Actually make move.
 
     def minimax(self, board, maximizing_player):
         available_moves = board.available_moves()
